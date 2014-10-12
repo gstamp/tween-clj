9c8248d2ce77479330f030402e1e0b814231b71c
Glen Stampoultzis <glen.stampoultzis@envato.com>
2014-10-12 13:37:48 +1100

    Bump the version and add some extras to the project file

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject tween-clj "0.4.0"
+(defproject tween-clj "0.5.0"
   :description "Tweening library for clojure"
   :url "https://github.com/gstamp/tween-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :warn-on-reflection true)
-
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
+                             [org.clojure/tools.trace "0.7.8"]]}}
+  :global-vars {*warn-on-reflection* true
+                *assert* true}
+  )
