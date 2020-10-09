e1d7869c7fba1258ebc8dbaf401bb559c3b67277
Glen Stampoultzis <gstamp@users.noreply.github.com>
2020-10-09 05:57:47 +0000

    Remove cider from dev dependencies.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.28"]]
-  :profiles {:dev {:plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]
-                             [org.clojure/tools.trace "0.7.8"]]}}
+  :profiles {:dev {:plugins [[org.clojure/tools.trace "0.7.8"]]}}
   :global-vars {*warn-on-reflection* true
                 *assert* true})
